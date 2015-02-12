b4087022a106275f94270cc3b696f858406bda3d
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2015-02-12 21:15:58 +1300

    Initial synchronous implementation.

---
+++ lib/utopia/websocket.rb
@@ -0,0 +1,81 @@
+# Copyright, 2015, by Samuel G. D. Williams. <http://www.codeotaku.com>
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+require "utopia/websocket/version"
+require 'websocket/driver'
+
+module Utopia
+	def self.WebSocket?(env)
+		::WebSocket::Driver.websocket?(env)
+	end
+	
+	module WebSocket
+		class Connection
+			attr_reader :env, :url
+			
+			def initialize(env, io)
+				@env = env
+				@io = io
+				
+				scheme = Rack::Request.new(env).ssl? ? 'wss:' : 'ws:'
+				@url = scheme + '//' + env['HTTP_HOST'] + env['REQUEST_URI']
+				
+				@driver = ::WebSocket::Driver.rack(self)
+				@running = false
+			end
+			
+			def write(string)
+				@io.write(string)
+			end
+			
+			def read(*args)
+				@driver.parse(@io.read(*args))
+			end
+			
+			def run(&handler)
+				@running = true
+				
+				@driver.on(:close) do
+					@running = false
+				end
+				
+				@driver.start
+				
+				yield @driver if block_given?
+				
+				while @running
+					self.read
+				end
+			end
+		end
+		
+		def self.open(env)
+			if ::WebSocket::Driver.websocket?(env)
+				env['rack.hijack'].call
+				
+				connection = Connection.new(env, env['rack.hijack_io'])
+				
+				connection.run do |driver|
+					yield driver
+				end
+			end
+		end
+	end
+end
---
+++ lib/utopia/websocket/version.rb
@@ -0,0 +1,5 @@
+module Utopia
+  module WebSocket
+    VERSION = "0.1.0"
+  end
+end
