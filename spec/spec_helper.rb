085840808a6491dabaccdf61e6846d82ddb5f469
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2019-02-09 12:52:14 +1300

    Update rspec config.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,6 +6,8 @@ require "async/websocket"
 require "async/rspec"
 
 RSpec.configure do |config|
+	config.disable_monkey_patching!
+	
 	# Enable flags like --only-failures and --next-failure
 	config.example_status_persistence_file_path = ".rspec_status"
 
