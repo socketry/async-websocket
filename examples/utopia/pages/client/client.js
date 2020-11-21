105ab914d4c132e7d4e0dec0e8da1c5f35a16e46
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2020-11-21 13:50:03 +1300

    Add ability to reconnect to server if it is disconnected.

--- examples/utopia/pages/client/client.js
+++ examples/utopia/pages/client/client.js
@@ -1,30 +1,48 @@
 
+function connectToChatServer(url) {
+	console.log("Connecting to server", url);
+	var server = new WebSocket(url.href);
+
+	server.onopen = function(event) {
+		console.log("Connected to", server);
+		chat.disabled = false;
+		
+		chat.onkeypress = function(event) {
+			if (event.keyCode == 13) {
+				server.send(JSON.stringify({text: chat.value}));
+				
+				chat.value = "";
+			}
+		}
+	};
+
+	server.onmessage = function(event) {
+		console.log("Got message", event);
+		
+		var message = JSON.parse(event.data);
+		
+		var pre = document.createElement('pre');
+		pre.innerText = message.text;
+		
+		response.appendChild(pre);
+	};
+
+	server.onerror = function(event) {
+		console.log("WebSocket Error", event);
+		chat.disabled = true;
+		server.close();
+	};
+
+	server.onclose = function(event) {
+		console.log("WebSocket Close", event);
+		
+		setTimeout(function() {
+			connectToChatServer(url);
+		}, 1000);
+	};
+}
+
 var url = new URL('/server/connect', window.location.href);
 url.protocol = url.protocol.replace('http', 'ws');
 
-console.log("Connecting to server", url);
-var server = new WebSocket(url.href);
-
-server.onopen = function(event) {
-	console.log("Connected to", server);
-	chat.disabled = false;
-	
-	chat.onkeypress = function(event) {
-		if (event.keyCode == 13) {
-			server.send(JSON.stringify({text: chat.value}));
-			
-			chat.value = "";
-		}
-	}
-};
-
-server.onmessage = function(event) {
-	console.log("Got message", event);
-	
-	var message = JSON.parse(event.data);
-	
-	var pre = document.createElement('pre');
-	pre.innerText = message.text;
-	
-	response.appendChild(pre);
-};
+connectToChatServer(url);
