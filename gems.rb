52a0feebaaee30caceaa3b8011094fbe9962220c
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2022-08-07 17:29:18 +1200

    Ensure gems are available to test workflows.

--- gems.rb
+++ gems.rb
@@ -5,9 +5,11 @@ gemspec
 group :maintenance, optional: true do
 	gem "bake-gem"
 	gem "bake-modernize"
-
-	gem "bake-test"
-	gem "bake-test-external"
 	
 	gem "utopia-project"
+end
+
+group :test do
+	gem "bake-test"
+	gem "bake-test-external"
 end
