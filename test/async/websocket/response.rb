d393490bba08346fd43b28e94c2af24fd6aab569
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2023-02-03 17:33:58 +1300

    Add test coverage for Response.

---
+++ test/async/websocket/response.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+# Released under the MIT License.
+# Copyright, 2023, by Samuel Williams.
+
+require 'async/websocket/response'
+
+describe Async::WebSocket::Response do
+	it "fails if the version is not recognized" do
+		request = Protocol::HTTP::Request.new(nil, nil, "GET", "/", "frob/2.0")
+		
+		expect do
+			subject.for(request)
+		end.to raise_exception(Async::WebSocket::ProtocolError, message: be =~ /Unsupported HTTP version/)
+	end
+end
